23676a06d5e62a914eda357e4d5200d6c432d948
Niranjanraj S <niranjanrajs1211@gmail.com>
2026-09-19 10:57:44 +0530

    Add Week 7 Problem 3 Orchestra Warm Up

---
+++ Class Problem/OrchestraWarmUp.java
@@ -0,0 +1,36 @@
+abstract class Instrument {
+    public abstract String play();
+}
+
+class StringInstrument extends Instrument {
+
+    public StringInstrument() {
+    }
+
+    @Override
+    public String play() {
+        return "Strumming the strings";
+    }
+}
+
+class Violin extends StringInstrument {
+
+    public Violin() {
+        super();
+    }
+
+    @Override
+    public String play() {
+        return super.play() + ", with a bow drawn across four strings";
+    }
+}
+
+public class OrchestraWarmUp {
+    public static void main(String[] args) {
+        StringInstrument s = new StringInstrument();
+        Violin v = new Violin();
+
+        System.out.println(s.play());
+        System.out.println(v.play());
+    }
+}
