4b11f75868827a6690f38b1ad7a3c83496e74fdf
Niranjanraj S <niranjanrajs1211@gmail.com>
2026-09-25 21:07:06 +0530

    Add Gallery Description Cards solution

---
+++ Assignment Problem/Problem2_GalleryDescriptionCards.java
@@ -0,0 +1,48 @@
+abstract class ArtPiece {
+    private static int counter = 0;
+    private final String pieceId;
+
+    public ArtPiece() {
+        pieceId = "P" + (++counter);
+    }
+
+    public abstract String describe();
+
+    public String getPieceId() {
+        return pieceId;
+    }
+}
+
+class Painting extends ArtPiece {
+    private String title;
+
+    public Painting(String title) {
+        this.title = title;
+    }
+
+    public String describe() {
+        return "Painting: " + title + ", framed on canvas";
+    }
+}
+
+class Sculpture extends ArtPiece {
+    private String title;
+
+    public Sculpture(String title) {
+        this.title = title;
+    }
+
+    public String describe() {
+        return "Sculpture: " + title + ", carved from stone";
+    }
+}
+
+public class Problem2_GalleryDescriptionCards {
+    public static void main(String[] args) {
+        Painting p = new Painting("Sunset Fields");
+        Sculpture s = new Sculpture("The Thinker II");
+
+        System.out.println(p.describe());
+        System.out.println(s.describe());
+    }
+}
